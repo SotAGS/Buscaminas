4c4c57b156b22415e4f773ec248358769116bec0
BarbaMateo3am <barbamateo89@gmail.com>
2025-07-29 18:10:14 -0300

    js contacto terminado y cambios al css

--- js/contacto.js
+++ js/contacto.js
@@ -3,15 +3,12 @@
     form.addEventListener('submit', function (e) {
       e.preventDefault();
 
-      const nombre = encodeURIComponent(document.getElementById('nombre').value);
-      const email = encodeURIComponent(document.getElementById('email').value);
-      const mensaje = encodeURIComponent(document.getElementById('mensaje').value);
+      var nombre = encodeURIComponent(document.getElementById('nombre').value);
+      var email = encodeURIComponent(document.getElementById('email').value);
+      var mensaje = encodeURIComponent(document.getElementById('mensaje').value);
 
-      const asunto = `Consulta de ${nombre}`;
-      const cuerpo = `Nombre: ${nombre}%0ACorreo: ${email}%0A%0AMensaje:%0A${mensaje}`;
+      var asunto = 'Consulta de '+ nombre;
+      const cuerpo = 'Nombre: ' + nombre + '%0ACorreo: ' + email + '%0A%0AMensaje:%0A' + mensaje;
 
-      window.location.href = `mailto:barbamateo89@gmail.com?subject=${asunto}&body=${cuerpo}`;
+      window.location.href = 'mailto:barbamateo89@gmail.com?subject=' + asunto + 'body=' +cuerpo;
     });
-
-
-    //TIENE USO DE CONST ASI QUE HAY QUE CAMBIARLO A ES5
